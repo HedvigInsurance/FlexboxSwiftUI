ba8a17f34376c4388e8c039fd649a54da18dc204
Sam Pettersson <sam@sampettersson.com>
2022-08-19 13:25:10 +0200

    Formatting

--- Demo/DemoApp.swift
+++ Demo/DemoApp.swift
@@ -16,9 +16,24 @@ struct DemoApp: App {
                 node: Node(
                     size: Size(width: .percent(100), height: .percent(100)),
                     children: [
-                        Node(size: Size(width: .fixed(10), height: .auto), flexGrow: 1, flexShrink: 1, view: AnyView(Color.red)),
-                        Node(size: Size(width: .fixed(10), height: .auto), flexGrow: 1, flexShrink: 1, view: AnyView(Color.blue)),
-                        Node(size: Size(width: .fixed(10), height: .auto), flexGrow: 1, flexShrink: 1, view: AnyView(Color.green)),
+                        Node(
+                            size: Size(width: .fixed(10), height: .auto),
+                            flexGrow: 1,
+                            flexShrink: 1,
+                            view: AnyView(Color.red)
+                        ),
+                        Node(
+                            size: Size(width: .fixed(10), height: .auto),
+                            flexGrow: 1,
+                            flexShrink: 1,
+                            view: AnyView(Color.blue)
+                        ),
+                        Node(
+                            size: Size(width: .fixed(10), height: .auto),
+                            flexGrow: 1,
+                            flexShrink: 1,
+                            view: AnyView(Color.green)
+                        ),
                     ],
                     justifyContent: .center
                 )
