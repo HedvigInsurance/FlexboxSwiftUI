3daea4e5b7c6247723219f413cd474f37fd2dda3
Sam Pettersson <sam@sampettersson.com>
2023-01-11 12:52:14 +0100

    Bump Placement

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/sampettersson/Placement.git", from: "1.3.9")
+        .package(url: "https://github.com/sampettersson/Placement.git", from: "1.4.0")
     ],
     targets: [
         .target(
